c1be3311069a63443bf25202cfd9cda8f8f4c871
tayeb <tayeb@MacBook-Pro-de-tayeb.local>
2023-02-10 09:36:04 +0100

    first commit

---
+++ partials/head.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>Pizzeria</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="./assets/js/script.js" defer></script>
+<link rel="stylesheet" href="assets/css/style.css">
+
+<body>
---
+++ partials/nav.php
@@ -0,0 +1,13 @@
+<!-- Navbar (sit on top) -->
+<nav>
+    <label for="burger">
+      <img src="./assets/img/pizza-slice-solid.svg" alt="menu_pizza">
+    </label>
+    <input type="checkbox" name="burger" id="burger">
+    <ul id="navbar">
+      <li><a href="#">HOME</a></li>
+      <li><a href="#menu">MENU</a></li>
+      <li><a href="#about">ABOUT</a></li>
+      <li><a href="#contact">CONTACT</a></li>
+    </ul>
+  </nav>
